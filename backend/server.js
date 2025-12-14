0908c7718669a8c8c268d157e2fd2917f2efce41
balaganesh-v <vbalaganesh2007@gmail.com>
2025-12-14 09:46:33 +0530

    Update the backend to enable Cross-Origin Resource Sharing (CORS) so that the frontend can make API calls to it.”

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,13 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
+
+
 app.use(express.json());
 
 // Routes
