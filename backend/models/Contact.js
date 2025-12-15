8079562c340b978d773341a23c501bd2b1bccd49
balaganesh-v <vbalaganesh2007@gmail.com>
2025-12-15 14:51:36 +0530

    Modify the Contact model , email is not required When saving the contact .

--- backend/models/Contact.js
+++ backend/models/Contact.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 
 const contactSchema = new mongoose.Schema({
     name: { type: String, required: true, trim: true },
-    email: { type: String, required: true, unique: true, lowercase: true },
     phone: { type: String, required: true },
+    email: { type: String,  unique: true, lowercase: true },
     address: { type: String, default: '' },
     company: { type: String, default: '' },
     jobTitle: { type: String, default: '' },
