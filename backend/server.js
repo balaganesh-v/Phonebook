995ae54cab8a3f5377753e6f81342228cf034364
balaganesh-v <vbalaganesh2007@gmail.com>
2026-03-04 16:16:00 +0530

    Initialize the PORT variable to run the server in Backend

--- backend/server.js
+++ backend/server.js
@@ -51,6 +51,8 @@ io.use(async (socket, next) => {
 // SINGLE socket connection
 socketManager(io);
 
-server.listen(5001, () => {
-    console.log("Server running on http://localhost:5001");
+const PORT = process.env.PORT || 5001;
+
+server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });
